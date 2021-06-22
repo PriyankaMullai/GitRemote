9388c6bb003ad508e0d431a392050f78f3f47a9a
PriyankaMullai <nanthinimppm@gmail.com>
2021-06-22 04:39:51 -0700

    First commit

---
+++ Mmm/src/org/mm/Mgit.java
@@ -0,0 +1,38 @@
+package org.mm;
+
+public class Mgit {
+public static void main(String[] args) {
+	
+
+	String s= "Java";
+	 String[] split = s.split(s);
+	
+	int a=0;
+	if (a==0) {
+		System.out.println("a is:"+s);
+	}else {
+		System.out.println("a is out of a");
+	}
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
